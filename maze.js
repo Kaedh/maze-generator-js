41212f87256e044614ab5218cb51feeab1d8be04
leonardo martins <leonardomds2013@gmail.com>
2021-01-21 13:36:30 -0300

    :sparkles: Implemented function that created the data structure

--- maze.js
+++ maze.js
@@ -1,13 +1,30 @@
 const gridDimensions = {
-    width: 15,
-    height: 15
+    width: 10,
+    height: 10
 };
 
 class Cell {
     constructor() {
-        top = true,
-        right = true,
-        bottom = true,
-        left = true
+        this.top = true,
+        this.right = true,
+        this.bottom = true,
+        this.left = true
     };
+};
+
+function createDataStructure(gridDimensions) {
+    let gridMatrix= [];
+
+    for (let row = 0; row < gridDimensions.height; row++) {
+        let newRow = [];
+        
+        for (let col = 0; col < gridDimensions.width; col++) {
+            newRow.push( new Cell() )
+        
+        }
+
+        gridMatrix.push(newRow);
+    };
+
+    return gridMatrix;
 };
