b943a41ca8639ca293abf71ac2b844d10ca424f4
leonardo martins <leonardomds2013@gmail.com>
2021-01-22 10:26:07 -0300

    :sparkles: code format

--- maze.js
+++ maze.js
@@ -1,55 +1,50 @@
 const gridDimensions = {
-    width: 10,
-    height: 10
+  width: 10,
+  height: 10,
 };
 
 class Cell {
-    constructor(index) {
-        this.top = true;
-        this.right = true;
-        this.bottom = true;
-        this.left = true;
+  constructor(index) {
+    this.top = true;
+    this.right = true;
+    this.bottom = true;
+    this.left = true;
 
-        this.visited = false;
+    this.visited = false;
 
-        this.index = index;
-    };
+    this.index = index;
+  }
 
-    markAsVisited() {
-        this.visited = true;
-    };
-
-};
+  markAsVisited() {
+    this.visited = true;
+  }
+}
 
 function createDataStructure(gridDimensions) {
-    let gridMatrix= [];
+  let gridMatrix = [];
 
-    for (let row = 0; row < gridDimensions.height; row++) {
-        let newRow = [];
-        
-        for (let col = 0; col < gridDimensions.width; col++) {
-            newRow.push( new Cell([row, col]) )
-        
-        }
+  for (let row = 0; row < gridDimensions.height; row++) {
+    let newRow = [];
 
-        gridMatrix.push(newRow);
-    };
+    for (let col = 0; col < gridDimensions.width; col++) {
+      newRow.push(new Cell([row, col]));
+    }
 
-    return gridMatrix;
-};
+    gridMatrix.push(newRow);
+  }
 
+  return gridMatrix;
+}
 
 function main() {
-    let stack = [];
-    const maze = createDataStructure(gridDimensions);
+  let stack = [];
+  const maze = createDataStructure(gridDimensions);
 
-    const initialCell = maze[0][0];
+  const initialCell = maze[0][0];
 
-    initialCell.markAsVisited();
+  initialCell.markAsVisited();
 
-    stack.push(initialCell);
-
-
+  stack.push(initialCell);
 }
 
 main();
